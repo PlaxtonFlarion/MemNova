6fe984496751addc3e61b7ce7dc857b367434224
AceKeppel <AceKeppel@outlook.com>
2025-04-01 01:08:11 +0800

    bug fixes

--- engine/device.py
+++ engine/device.py
@@ -1,7 +1,15 @@
+import re
+
+
 class Device(object):
 
     def __init__(self, serial: str):
         self.serial = serial
+
+    def __str__(self):
+        return f"<Device serial={self.serial}>"
+
+    __repr__ = __str__
 
 
 if __name__ == '__main__':
