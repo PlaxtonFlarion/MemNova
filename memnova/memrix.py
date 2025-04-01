335f88dd0650663fd17985a2124afb42c15294e1
AceKeppel <AceKeppel@outlook.com>
2025-04-02 00:15:59 +0800

    bug fixes

--- memnova/memrix.py
+++ memnova/memrix.py
@@ -1,3 +1,16 @@
+#  __  __                     _
+# |  \/  | ___ _ __ ___  _ __(_)_  __
+# | |\/| |/ _ \ '_ ` _ \| '__| \ \/ /
+# | |  | |  __/ | | | | | |  | |>  <
+# |_|  |_|\___|_| |_| |_|_|  |_/_/\_\
+#
+# 版权所有 (c) 2024  Memrix(记忆星核)
+# 此文件受 Memrix(记忆星核) 许可证的保护。您可以在 LICENSE.md 文件中查看详细的许可条款。
+#
+# Copyright (c) 2024  Memrix(记忆星核)
+# This file is licensed under the Memrix(记忆星核) License. See the LICENSE.md file for more details.
+#
+
 import os
 import re
 import sys
@@ -12,7 +25,6 @@ import aiosqlite
 from loguru import logger
 from collections import Counter
 from engine import const
-from engine.device import Device
 from engine.manage import Manage
 from engine.parser import Parser
 from engine.tackle import Config, Log, DataBase, Terminal, RAM, MemrixError, ReadFile
