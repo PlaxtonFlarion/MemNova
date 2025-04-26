c5e83e20761ded6a86ca9e2b1e6282ebd14aaf46
AceKeppel <AceKeppel@outlook.com>
2025-04-27 00:10:34 +0800

    bug fixes

--- build.py
+++ build.py
@@ -313,7 +313,8 @@ if __name__ == "__main__":
         asyncio.run(post_build())
     except MemrixError as _e:
         compile_log(_e)
-        sys.exit(Display.show_fail())
+        Display.show_fail()
+        sys.exit(1)
     except KeyboardInterrupt:
         sys.exit(Display.show_exit())
     else:
