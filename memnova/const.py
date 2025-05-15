9b82b37276983f0ad67ae08ed0a80f2e9079ba4d
AceKeppel <AceKeppel@outlook.com>
2025-05-15 23:26:54 +0800

    bug fixes

--- memnova/const.py
+++ memnova/const.py
@@ -11,43 +11,43 @@
 # This file is licensed under the Memrix(记忆星核) License. See the LICENSE.md file for more details.
 #
 
-APP_ITEM: str = r"MemNova"
-APP_NAME: str = r"memrix"
-APP_DESC: str = r"Memrix"
-APP_CN: str = r"记忆星核"
-APP_VERSION: str = r"1.0.0"
-APP_LICENSE: str = r"MIT"
-APP_URL: str = r"https://github.com/PlaxtonFlarion/MemNova"
-APP_DECLARE: str = f"""\
+APP_ITEM = r"MemNova"
+APP_NAME = r"memrix"
+APP_DESC = r"Memrix"
+APP_CN = r"记忆星核"
+APP_VERSION = r"1.0.0"
+APP_LICENSE = r"MIT"
+APP_URL = r"https://github.com/PlaxtonFlarion/MemNova"
+APP_DECLARE = f"""\
 [bold][bold #FF8787](c)[/] [bold #FFAA33]2024[/] [bold #00D7AF]{APP_DESC} :: {APP_CN}[/]
 Licensed under the Starbound Intelligence Charter.
 Redistribute freely :: let memory echo through every fork.[/]
 """
 
-AUTHOR: str = r"AceKeppel"
-EMAIL: str = r"AceKeppel@outlook.com"
+AUTHOR = r"AceKeppel"
+EMAIL = r"AceKeppel@outlook.com"
 
-ENCODING: str = f"UTF-8"
+ENCODING = f"UTF-8"
 
 SUC = f"[bold #FFFFFF on #32CD32]"
 WRN = f"[bold #000000 on #FFFF00]"
 ERR = f"[bold #FFFFFF on #FF6347]"
 
-TOTAL_DIR: str = f"{APP_DESC}_Library"
-SUBSET_DIR: str = f"Tree"
-DB_FILE: str = f"{APP_NAME}_data.db"
+TOTAL_DIR = f"{APP_DESC}_Library"
+SUBSET_DIR = f"Tree"
+DB_FILE = f"{APP_NAME}_data.db"
 
-SUMMARY: str = f"Summary"
+SUMMARY = f"Summary"
 
-SCHEMATIC: str = f"schematic"
-STRUCTURE: str = f"Structure"
+SCHEMATIC = f"schematic"
+STRUCTURE = f"Structure"
 
-SRC_OPERA_PLACE: str = f"{APP_DESC}_Mix"
-SRC_TOTAL_PLACE: str = f"{APP_DESC}_Report"
-CONFIG: str = f"{APP_NAME}_config.yaml"
-LIC_FILE: str = f"{APP_NAME}_signature.lic"
+SRC_OPERA_PLACE = f"{APP_DESC}_Mix"
+SRC_TOTAL_PLACE = f"{APP_DESC}_Report"
+CONFIG = f"{APP_NAME}_config.yaml"
+LIC_FILE = f"{APP_NAME}_signature.lic"
 
-DISPLAY_LEVEL: str = f"WARNING"
+DISPLAY_LEVEL = f"WARNING"
 
 PRINT_HEAD = f"[bold #EEEEEE]{APP_DESC} ::[/]"
 OTHER_HEAD = f"{APP_DESC} ::"
