bb8a5ce214e2088260698af0b7b0f68e21c75a7c
AceKeppel <144319755+PlaxtonFlarion@users.noreply.github.com>
2025-07-30 13:11:25 +0800

    bug fixes

--- engine/device.py
+++ engine/device.py
@@ -32,7 +32,7 @@ class Device(object):
         self.device_info = kwargs
 
     def __str__(self):
-        return f"<Device {self.device_info}>"
+        return f"<Device {self.device_info['brand']} serial={self.serial}>"
 
     __repr__ = __str__
 
